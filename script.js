e0f8bd7ba9449e9ca5ab015b6b19570c0dea5b68
emrekocak75 <emre.kocak.ac@gmail.com>
2025-04-13 18:32:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// Şifreyi kontrol et
 function checkPassword() {
   const password = document.getElementById('password').value;
-  const correctPassword = '010621'; // Şifreyi buraya gir
+  const correctPassword = '010621';
 
   if (password === correctPassword) {
-    // Giriş başarılı, galeriyi göster
     document.getElementById('login').style.display = 'none';
     document.getElementById('gallery').style.display = 'block';
 
-    // Müzik çalmaya başlasın
     const music = document.getElementById('background-music');
-    music.play(); // Müzik başlasın
+    music.play();
   } else {
-    // Şifre yanlışsa hata mesajını göster
     document.getElementById('error-message').style.display = 'block';
   }
 }
